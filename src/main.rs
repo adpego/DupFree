3aced3ff5762e526c62332f14d5ebb4fa4e50e45
adpego <adpego7@gmail.com>
2024-09-16 20:24:45 +0200

    Storing the file into struct and get the saved sized

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ use hex;
 use std::time::Instant;
 use crc64;
 use std::collections::HashMap;
+use human_bytes::human_bytes;
 
+struct FileInfo {
+    file_path: String,
+    size: u64,
+}
 // Calculate sha256 from file
 fn sha256_from_file(file_path: &str) -> String {
     let mut file = File::open(file_path).unwrap();
@@ -34,10 +39,11 @@ fn crc64_from_file(file_path: &str) -> String {
 fn main() {
 
     // Hashmap with the crc and list of files
-    let mut crc_map: HashMap<String, Vec<String>> = HashMap::new();
+    let mut crc_map: HashMap<String, Vec<FileInfo>> = HashMap::new();
     let path = ".";
     let now = Instant::now();
-
+    let mut total_size = 0;
+    let mut reduced_size = 0;
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_dir() {
             continue;
@@ -46,7 +52,14 @@ fn main() {
         let file_path = entry.path().to_str().unwrap();
 
         let crc64 = crc64_from_file(file_path);
-        crc_map.entry(crc64).or_insert(Vec::new()).push(file_path.to_string());
+        let file_info = FileInfo {
+            file_path: file_path.to_string(),
+            size: entry.metadata().unwrap().len(),
+        };
+        total_size += file_info.size;
+
+
+        crc_map.entry(crc64).or_insert(Vec::new()).push(file_info);
 
     }
 
@@ -54,12 +67,23 @@ fn main() {
 
     // print the crc and the list of files
     for (crc, files) in crc_map.iter() {
+        reduced_size += files[0].size;
+        if files.len() == 1 {
+            continue;
+        }
         println!("CRC64: {}", crc);
+        if files.len() > 1 {
+            println!("Files:");
+        }
         for file in files {
-            println!("{}", file);
+            println!("{} {}", file.file_path, human_bytes(file.size as f64));
         }
         println!("\n");
     }
 
-    println!("Elapsed: {:.2?}\n", elapsed);
+    println!("Total size: {}", human_bytes(total_size as f64));
+    println!("Reduced size: {}", human_bytes(reduced_size as f64));
+    println!("Size saved: {}", human_bytes((total_size - reduced_size) as f64));
+
+    println!("\nElapsed: {:.2?}\n", elapsed);
 }
